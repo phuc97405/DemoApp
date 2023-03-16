76c09d7ed4ad38fe7b9c6ec0e7d9e48b1a20d8f5
Lê Ngọc Phúc <74225031+phuc97405@users.noreply.github.com>
2023-03-16 13:08:47 +0700

    add save data

--- src/modules/calendar/Calendar.tsx
+++ src/modules/calendar/Calendar.tsx
@@ -2,18 +2,19 @@ import React, { useEffect, useRef, useState } from "react";
 import styles from "./styles";
 import { Calendar } from "react-native-calendars";
 import {
-  FlatList,
   Text,
   View,
   TouchableOpacity,
   ScrollView,
+  Alert,
+  ToastAndroid,
 } from "react-native";
 import ModalNotion from "~components/modal/ModalNotion";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { TextInput } from "react-native-gesture-handler";
 
 const listNameSort = [
-  { title: "All", id: 1, color: "black" },
+  { title: "All", id: 0, color: "black" },
   { title: "Started", id: 1, color: "green" },
   { title: "In Progress", id: 2, color: "yellow" },
   { title: "Finished", id: 3, color: "blue" },
@@ -24,21 +25,31 @@ const CalendarScreen = () => {
   const [isVisible, setIsVisible] = useState<boolean>(false);
   const [listItemChoose, setItemChoose] = useState<any[]>([...listNameSort]);
   const [dateString, setDateString] = useState<string>("");
-  const currentIdChoose = useRef<number>(0);
-  const vacation = { key: "vacation", color: "blue" };
-  const massage = { key: "massage", color: "red" };
-  const workout = { key: "workout", color: "green" };
-  const [dataCalendar, setDataCalendar] = useState<any[]>();
+  const currentIdChoose = useRef<number | undefined>(undefined);
+  const [dataCalendar, setDataCalendar] = useState<any[]>([]);
   const [inputText, setInputText] = useState<string>("");
+  const [objetRender, setObjectRender] = useState<{}>({});
 
   const loadCalendar = async () => {
     const jsonString = await AsyncStorage.getItem("demo-app");
     const jsonObject = jsonString ? JSON.parse(jsonString) : "";
-    setDataCalendar([jsonObject]);
-    console.log(jsonObject);
-    // Object.assign(this, jsonObject);
+    const result: any = {};
+
+    for (const item of jsonObject) {
+      const date = Object.keys(item)[0];
+      const color = item[date].color;
+      const key = item[date].key;
+      if (result[date]) {
+        result[date].dots.push({ key, color });
+      } else {
+        result[date] = { dots: [{ key, color }] };
+      }
+    }
+    console.log("jjasdas", { ...objetRender });
+    setDataCalendar(jsonObject);
+    setObjectRender(result);
   };
-  const saveCalendar = async (data: {}) => {
+  const saveCalendar = async (data: any) => {
     let jsonString = "";
     jsonString = JSON.stringify(data); //value
     await AsyncStorage.setItem("demo-app", jsonString);
@@ -54,9 +65,10 @@ const CalendarScreen = () => {
           <Text style={styles.txtBtn}>Create Event</Text>
         </TouchableOpacity>
 
-        <ScrollView showsHorizontalScrollIndicator={false} horizontal>
-          {listNameSort.map((item) => (
+        <ScrollView showsHorizontalScrollIndicator horizontal={true}>
+          {listNameSort.map((item, index) => (
             <TouchableOpacity
+              key={index}
               style={[styles.btn, { backgroundColor: item.color }]}
             >
               <Text style={[styles.txtBtn, { color: "#E0E0E0" }]}>
@@ -69,63 +81,81 @@ const CalendarScreen = () => {
       <Calendar
         markingType={"multi-dot"}
         markedDates={{
-          "2023-03-25": {
-            dots: [vacation, massage, workout],
-            // selected: true,
-            // selectedColor: "red",
-          },
-          "2023-03-16": { dots: [massage, workout], disabled: false },
+          ...objetRender,
+          // "2023-03-25": {
+          //   dots: [vacation, massage, workout],
+          //   // selected: true,
+          //   // selectedColor: "red",
+          // },
+          // "2023-03-16": { dots: [ { key: "vacation", color: "blue" }] },
+          // ...Object.keys(dataCalendar),
         }}
         onDayPress={(value) => {
           setDateString(value?.dateString);
-          console.log(value?.dateString);
           setIsVisible(true);
         }}
       />
       <ModalNotion
-        onPressCancel={() => {
-          setIsVisible(false);
-        }}
-        onPressOk={() => {
-          saveCalendar({
-            [dateString]: [
-              { title: "testTitle", status: currentIdChoose.current },
-            ],
-          });
-          setIsVisible(false);
-        }}
+        onPressCancel={() => setIsVisible(false)}
+        onPressOk={
+          inputText && currentIdChoose.current
+            ? async () => {
+                await saveCalendar([
+                  ...dataCalendar,
+                  {
+                    [dateString]: {
+                      key: inputText,
+                      color: listItemChoose[currentIdChoose.current!]?.color,
+                    },
+                  },
+                ]);
+                setObjectRender({
+                  ...objetRender,
+                  [dateString]: {
+                    key: inputText,
+                    color: listItemChoose[currentIdChoose.current!]?.color,
+                  },
+                });
+                setIsVisible(false);
+                setInputText("");
+                currentIdChoose.current = undefined;
+              }
+            : () => {}
+        }
         title={"Set Calendar"}
         isVisible={isVisible}
         children={
           <View style={{ marginVertical: 30 }}>
             <View style={{ flexDirection: "row" }}>
               <Text style={styles.txtPopup}>Status: </Text>
-              {listItemChoose.map((i, index) => (
-                <TouchableOpacity
-                  key={index}
-                  onPress={() => {
-                    currentIdChoose.current = i.id;
-                    setItemChoose((preState) => {
-                      const newArr = [...preState];
-                      newArr.forEach((item, i) => {
-                        if (item !== i) {
-                          item.isCheck = false;
-                        }
+              {listItemChoose.map((i, index) =>
+                index > 0 ? (
+                  <TouchableOpacity
+                    key={index}
+                    onPress={() => {
+                      currentIdChoose.current = i.id;
+                      setItemChoose((preState) => {
+                        const newArr = [...preState];
+                        newArr.forEach((item, i) => {
+                          if (item !== i) {
+                            item.isCheck = false;
+                          }
+                        });
+                        newArr[index].isCheck = true;
+                        return newArr;
                       });
-                      newArr[index].isCheck = true;
-                      return newArr;
-                    });
-                  }}
-                  style={[
-                    i?.isCheck == true
-                      ? styles.tcb_Choose_border
-                      : styles.tcb_Choose,
-                    { backgroundColor: i.color },
-                  ]}
-                >
-                  <Text />
-                </TouchableOpacity>
-              ))}
+                    }}
+                    style={[
+                      i?.isCheck == true
+                        ? styles.tcb_Choose_border
+                        : styles.tcb_Choose,
+                      { backgroundColor: i.color },
+                    ]}
+                  >
+                    <Text />
+                  </TouchableOpacity>
+                ) : null
+              )}
             </View>
             <Text style={styles.txtPopup}>DateTime: {dateString} </Text>
             <View style={{ flexDirection: "row", alignItems: "center" }}>
